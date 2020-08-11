c1ea33544c480761bc4989c7656d679c19960965
Dev C Krishna <dev.c.krishna.123@gmail.com>
2020-08-11 14:07:57 +0530

    initial buil

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:amazon_ui/screens/overview_screen.dart';
+import 'package:flutter/material.dart';
+
+void main()=>runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:OverviewScreen() ,
+    );
+  }
+}
